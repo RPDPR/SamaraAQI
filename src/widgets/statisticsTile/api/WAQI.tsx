3a0edb58cae3cf3e4c9732be5faafa95b61939f8
Igor Krotinov <dancigor@gmail.com>
2025-08-31 21:40:02 +0400

    resultValue fix

--- src/widgets/statisticsTile/api/WAQI.tsx
+++ src/widgets/statisticsTile/api/WAQI.tsx
@@ -134,7 +134,7 @@ export const WAQI: FC<T_WAQI> = ({ secondary }) => {
   return (
     <div className="relative w-110 h-full mx-auto lg:mx-0 2k:mx-0 4k:mx-0 min-h-[149.5px] 4k:min-h-[299px]">
       <h1 className="absolute text-[80px] lg:text-[200px] 4k:text-[400px] font-sans font-semibold top-[10] lg:top-[-10] 4k:top-[-20] lg:left-2 tracking-[-5px] lg:tracking-[-11px] 4k:tracking-[-22px] text-nowrap">
-        {res.resultValue ? "543" : "no data"}
+        {res.resultValue ? res.resultValue.toLocaleString() : "no data"}
       </h1>
       <h2 className="absolute text-[40px] lg:text-[40px] 4k:text-[80px] font-sans font-bold top-[110px] lg:top-54.5 4k:top-109 lg:left-[7px] 4k:left-[14px] tracking-[-2.7px] lg:tracking-[-2.8px] 4k:tracking-[-5.6px] lg:w-60 4k:w-120">
         {res.resultString ? res.resultString.toLocaleString() : ""}
