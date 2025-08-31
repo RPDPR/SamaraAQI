d13794ff8bcdac37f12bbc1bb67f1c11d3a9b1ed
Igor Krotinov <dancigor@gmail.com>
2025-08-31 17:12:02 +0400

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import { Header } from "@/widgets/header/index";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Header />
         <main className="flex-1 lg:px-10 4k:px-20 flex flex-col">
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
